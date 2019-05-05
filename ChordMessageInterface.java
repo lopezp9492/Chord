2da6e59b666a3cb9cbb31ef2932d96d3f6942c1e
lopezp <lopezp9492@gmail.com>
2019-05-05 08:26:47 -0700

    added callSuccesorToSendAll() to CMI

--- ChordMessageInterface.java
+++ ChordMessageInterface.java
@@ -19,16 +19,16 @@ public interface ChordMessageInterface extends Remote
     public byte[] get(long guidObject, long offset, int len) throws IOException, RemoteException;  
     public void delete(long guidObject) throws IOException, RemoteException;
 
-    public void onChordSize(long source, int n)throws RemoteException;
-    public void onPageCompleted(String file)throws RemoteException;
+    public void onChordSize(long source, int n) throws RemoteException;
+    public void onPageCompleted(String file) throws RemoteException;
     //public void mapContext(int page, Mapper mapper, ChordMessageInterface coordinator, String file);
     //public void reduceContext(int page, Mapper reducer, ChordMessageInterface coordinator, String file);
-    public void addKeyValue(long key, int value)throws RemoteException;
-    public void emit(long key, int value, String file)throws RemoteException; // Sends
-    public void bulk(int page)throws RemoteException;
+    public void addKeyValue(long key, int value) throws RemoteException;
+    public void emit(long key, int value, String file) throws RemoteException; // Sends
+    public void bulk(int page) throws RemoteException;
 
     public void map(String fileName, long guid) throws RemoteException;
-    public void store(RemoteInputFileStream rifs)throws RemoteException;
+    public void store(RemoteInputFileStream rifs) throws RemoteException;
     public void arePagesMapped(long source, String fileName, Boolean state, int n) throws RemoteException;
-
+    public void callSuccesorToSendAll(long source, int n) throws RemoteException;
 }
