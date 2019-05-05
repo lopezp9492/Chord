aa5865178cb5094353f8c9842a7c19c517134cba
lopezp <lopezp9492@gmail.com>
2019-05-05 16:10:40 -0700

    Todo comment

--- DFS.java
+++ DFS.java
@@ -1353,6 +1353,7 @@ public class DFS
         System.out.println("sendAll() Done.");
 
 
+        //TODO:  filter nodes in bulk
         /**
         //All peers bulk() //Save all nodes in TreeMap as their own file). 
         System.out.println("Calling Peers to bulk()");
