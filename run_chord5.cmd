ac0f1df0c4f417c74347eab96981cf686dbcabb7
lopezp <lopezp9492@gmail.com>
2019-05-06 12:07:59 -0700

    chord5 and chord9

---
+++ run_chord5.cmd
@@ -0,0 +1,5 @@
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 2020
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 3003
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 4004
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 5005
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 6006
---
+++ run_chord9.cmd
@@ -0,0 +1,9 @@
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 2020
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 3003
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 4004
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 5005
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 6006
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 7007
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 8008
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 9009
+start cmd /k java -cp "%cd%;%cd%\gson\gson-2.6.2.jar" DFSCommand 9010
